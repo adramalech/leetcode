4e15689e7fca2fe6cdfc12c9cccc95c94f554055
Jonathan Throne <jthrone@skyslope.com>
2020-04-12 17:12:52 -0700

    Initial commit.

---
+++ Fb.Test/ArraysStrings/StringProblemsTest.cs
@@ -0,0 +1,113 @@
+using Xunit;
+using Fb.ArraysStrings;
+
+namespace Fb.Test.ArraysStrings
+{
+    public class StringProblemsTests
+    {
+        [Fact]
+        public void GivingStringOfLengthOneShouldReturnSubstringLengthOne() {
+          var strProblems = new StringProblems();
+          const int expectedLength = 1;
+
+          var actualLength = strProblems.LengthOfLongestSubstring(" ");
+
+          Assert.Equal<int>(expectedLength, actualLength);
+        }
+
+        [Fact]
+        public void GivingUniqueStringOfLengthTwoShouldReturnSubstringLengthTwo() {
+          var strProblems = new StringProblems();
+          const int expectedLength = 2;
+
+          var actualLength = strProblems.LengthOfLongestSubstring("ab");
+
+          Assert.Equal<int>(expectedLength, actualLength);
+        }
+
+        [Fact]
+        public void GivingNonUniqueStringOfLengthTwoShouldReturnSubstringLengthTwo() {
+          var strProblems = new StringProblems();
+          const int expectedLength = 1;
+
+          var actualLength = strProblems.LengthOfLongestSubstring("aa");
+
+          Assert.Equal<int>(expectedLength, actualLength);
+        }
+
+        [Fact]
+        public void InputStringRepeatingCharacterShouldReturnSubstringLengthOne() {
+          var strProblems = new StringProblems();
+          const int expectedLength = 1;
+
+          var actualLength = strProblems.LengthOfLongestSubstring("aaaa");
+
+          Assert.Equal<int>(expectedLength, actualLength);
+        }
+
+        [Fact]
+        public void InputStringComplexShouldResultInSix() {
+          var strProblems = new StringProblems();
+          const int expectedLength = 6;
+
+          var actualLength = strProblems.LengthOfLongestSubstring("ohvhjdml");
+
+          Assert.Equal<int>(expectedLength, actualLength);
+        }
+
+        [Fact]
+        public void InputNumberShouldResultInIntValueEvenWithJunkFollowing()
+        {
+          var strProblems = new StringProblems();
+          const int expectedValue = 2342;
+
+          var actualLength = strProblems.Atoi(" +2342 h  -222222222 here i am test!");
+          
+          Assert.Equal<int>(expectedValue, actualLength);
+        }
+
+        [Fact]
+        public void InputNumberShouldResultInIntValueWhenSurroundedByWhitespace()
+        {
+          var strProblems = new StringProblems();
+          const int expectedValue = 2342;
+
+          var actualLength = strProblems.Atoi("    2342   ");
+          
+          Assert.Equal<int>(expectedValue, actualLength);
+        }
+
+        [Fact]
+        public void InputNumberIfTooLongWillReturnMinValue()
+        {
+          var strProblems = new StringProblems();
+          const int expectedValue = int.MinValue;
+
+          var actualLength = strProblems.Atoi("-12345678900");
+          
+          Assert.Equal<int>(expectedValue, actualLength);
+        }
+        
+        [Fact]
+        public void InputNumberIfTooLongWillReturnMaxValue()
+        {
+          var strProblems = new StringProblems();
+          const int expectedValue = int.MaxValue;
+
+          var actualLength = strProblems.Atoi("2147483649");
+          
+          Assert.Equal<int>(expectedValue, actualLength);
+        }
+
+        [Fact]
+        public void InputNumberZerosShouldDropOff()
+        {
+          var strProblems = new StringProblems();
+          const int expectedValue = 0;
+          
+          var actualLength = strProblems.Atoi("0-1");
+          
+          Assert.Equal<int>(expectedValue, actualLength);
+        }
+    }
+}
---
+++ Fb/ArraysStrings/StringProblems.cs
@@ -0,0 +1,289 @@
+using System.Collections.Generic;
+
+namespace Fb.ArraysStrings
+{
+  public class StringProblems
+  {
+    public int LengthOfLongestSubstring(string s)
+    {
+      // base case if input string is null, empty or whitespace length is 0.
+      if (string.IsNullOrEmpty(s))
+      {
+        return 0;
+      }
+
+      int maxLength = s.Length;
+      int left = 0;
+      int right = 0;
+      var uniqueString = new HashSet<char>();
+      int totalSubStringLength = 0;
+      int uniqueStringCount = 0;
+
+      // base case if string is length one.
+      if (maxLength == 1)
+      {
+        return 1;
+      }
+
+      // base case if string is length 2 then if characters don't match return 2 else return 1.
+      if (maxLength <= 2)
+      {
+        if (s[0] != s[1])
+        {
+          return 2;
+        }
+        
+        return 1;
+      }
+      
+      // iterate once through original input (O(n))
+      while (left < maxLength && right < maxLength )
+      {
+        // resolve substring unique conflict.
+        while (!uniqueString.Add(s[right]))
+        {
+          // remove the far left character
+          uniqueString.Remove(s[left]);
+          // decrease size by one
+          uniqueStringCount--;
+          // shrink window by one
+          left++;
+        }
+
+        // add new character.
+        uniqueStringCount++;
+        // move to grow window size right by one.
+        right++;
+
+        // update total count tracking the maximum length unique sub-string.
+        if (totalSubStringLength < uniqueStringCount)
+        {
+          totalSubStringLength = uniqueStringCount;
+        }
+      }
+
+      return totalSubStringLength;
+    }
+
+    public int Atoi(string s)
+    {
+      // base case s is null or empty return 0
+      if (string.IsNullOrEmpty(s))
+      {
+        return 0;
+      }
+      
+      var countDigits = 0;
+      var haveSeenDigit = false;
+      var haveSeenSign = false;
+      var haveSeenNegativeSign = false;
+      var haveSeenNonDigit = false;
+      var haveSeenLeadingZero = false;
+      long num = 0;
+      var stack = new Stack<char>();
+
+      // iterate through each character worst case O(n)
+      foreach (char c in s)
+      {
+        // if we have seen a non-digit character stop iterating.
+        if (haveSeenNonDigit)
+        {
+          break;
+        }
+
+        int tmpNum = -1;
+
+        switch (c)
+        {
+          // whitespace skip
+          case ' ':
+            if (haveSeenLeadingZero)
+            {
+              return 0;
+            }
+            
+            // if we have seen a + or - but not a digit and now seeing space error!
+            if (haveSeenSign && !haveSeenDigit)
+            {
+              return 0;
+            }
+            
+            // if whitespace found after encountering a digit return
+            if (haveSeenDigit)
+            {
+              haveSeenNonDigit = true;
+            }
+            break;
+          
+          // positive or negative sign is optional
+          case '+':
+          case '-':
+            if (haveSeenLeadingZero)
+            {
+              return 0;
+            }
+
+            // have seen non-digit!
+            if (haveSeenDigit)
+            {
+              haveSeenNonDigit = true;
+              continue;
+            }
+            
+            // if we have already seen a sign return 0.  like double sign.
+            // immediately following a sign should be a digit!
+            if (haveSeenSign)
+            {
+              return 0;
+            }
+            
+            haveSeenSign = true;
+            // determine if the sign is negative or positive.
+            haveSeenNegativeSign = (c == '-');
+            break;
+
+          case '0':
+            // if we have seen digits [1-9] then add 0s else skip.
+            if (!haveSeenDigit)
+            {
+              haveSeenLeadingZero = true;
+              continue;
+            }
+            
+            countDigits++;
+            
+            // stop if processed more than billionth digit.
+            if (countDigits > 10)
+            {
+              return ((haveSeenNegativeSign) ? int.MinValue : int.MaxValue);
+            }
+          
+            stack.Push(c);
+            break;
+          case '1':
+          case '2':
+          case '3':
+          case '4':
+          case '5':
+          case '6':
+          case '7':
+          case '8':
+          case '9':
+            // keep pushing digits on to the stack.
+            //stack.Push(c);
+            haveSeenDigit = true;
+            countDigits++;
+
+            // stop if processed more than billionth digit.
+            if (countDigits > 10)
+            {
+              return ((haveSeenNegativeSign) ? int.MinValue : int.MaxValue);
+            }
+            
+            stack.Push(c);
+            break;
+          
+          // any non-digit character or symbol not \d, \s, +, or - will end up here.
+          default:
+            // if we haven't seen digits yet this is a failure!
+            if (!haveSeenDigit)
+            {
+              return 0;
+            }
+
+            // make sure to signal to break the loop
+            haveSeenNonDigit = true;
+            break;
+        }
+      }
+      
+      // if we see a + or - sign and haven't seen a digit return 0
+      if (!haveSeenDigit || countDigits < 1)
+      {
+        return 0;
+      }
+
+      long i = 1;
+      // start adding 
+      while (stack.Count > 0)
+      {
+        long n = convertCharToDigit(stack.Pop());
+
+        // error found.
+        if (n == -1)
+        {
+          return 0;
+        }
+        
+        num += n * i;
+        i *= 10;
+      }
+
+      if (haveSeenNegativeSign)
+      {
+        num = -num;
+
+        // if value is lower than -2147483648 return min value.
+        if (num <= int.MinValue)
+        {
+          return int.MinValue;
+        }
+
+        return (int) num;
+      }
+
+      // if value is larger than 2147483647 return max value.
+      if (num >= int.MaxValue)
+      {
+        return int.MaxValue;
+      }
+      
+      // safe to typecast to 32-bit signed int.
+      return (int)num;
+    }
+
+    private long convertCharToDigit(char c)
+    {
+      int n;
+      
+      switch (c)
+      {
+        case '0':
+          n = 0;
+          break;
+        case '1':
+          n = 1;
+          break;
+        case '2':
+          n = 2;
+          break;
+        case '3':
+          n = 3;
+          break;
+        case '4':
+          n = 4;
+          break;
+        case '5':
+          n = 5;
+          break;
+        case '6':
+          n = 6;
+          break;
+        case '7':
+          n = 7;
+          break;
+        case '8':
+          n = 8;
+          break;
+        case '9':
+          n = 9;
+          break;
+        default:
+          n = -1;
+          break;
+      }
+
+      return n;
+    }
+  }
+}
