38558bc95503ac05bf4d57b889ddc2fae7143d8f
Jonathan Throne <jthrone@skyslope.com>
2020-05-09 16:17:43 -0700

    Added in some improvements to minimum substring solution.

--- Google/ArraysStrings/StringProblems.cs
+++ Google/ArraysStrings/StringProblems.cs
@@ -12,27 +12,29 @@ namespace Google.ArraysStrings
                 return string.Empty;
             }
             
-            // we have a starting search range.
+            // we want to initialize the window to the minimum length of T
             var left = 0;
-            var right = 0;
+            var right = t.Length - 1;
             var minSize = 0;
             var minLeft = 0;
-            var curSize = 1;
+            var curSize = t.Length;
             
-            // iterate while the left position has not proceeded beyond the right size
-            // and make sure  that the right side stays less than the maximum size of the input.
-            while (left <= right && right < s.Length) 
+            // iterate while:
+            //    1. the left position has not proceeded beyond the right size
+            //    2. making sure that the right side stays less than the maximum size of the input.
+            //    3. if the current window size must be greater than or equal to the length of T
+            while (left <= right && curSize >= t.Length && right < s.Length) 
             {
                 // check to see if this current window matches the symbols.
                 var areAllSymbolsFound = symbolsMatchString(s.Substring(left, curSize), t);
-                
+
                 // if they do match make sure to compare current minimum size and window.
                 // if it is less than the current minimum replace current minimum.
-                if (areAllSymbolsFound) 
+                if (areAllSymbolsFound)
                 {
                     // if the minimum size isn't matched or if it is minimum
                     // set the current window size and boundary positions as the new minimum.
-                    if (minSize == 0 || minSize > (right - left  + 1)) 
+                    if (minSize == 0 || minSize > (right - left + 1))
                     {
                         minSize = right - left + 1;
                         minLeft = left;
@@ -41,12 +43,12 @@ namespace Google.ArraysStrings
                     // increment the left to make sure that we cannot make it any smaller.
                     left++;
                 }
-                else 
+                else
                 {
                     // we were unable to find a match grow the window size.
                     right++;
                 }
-
+                
                 // set the current size of the window.
                 curSize = right - left + 1;
             }
