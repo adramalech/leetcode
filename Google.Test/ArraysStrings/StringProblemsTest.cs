48b68be73c351bcd78dc59e33869ff6964cbaa79
Jonathan Throne <jthrone@skyslope.com>
2020-05-09 18:39:33 -0700

    Got min substring solution in O(n) time.

--- Google.Test/ArraysStrings/StringProblemsTest.cs
+++ Google.Test/ArraysStrings/StringProblemsTest.cs
@@ -6,21 +6,11 @@ namespace Google.Test.ArraysStrings
     public class StringProblemsTest
     {
         [Theory]
-        [InlineData("abcdefhi", "afi", true)]
-        public void TestSymbolsMatchString(string s, string t, bool expectedResult)
-        {
-            var stringProblems = new StringProblems();
-
-            var actualResult = stringProblems.symbolsMatchString(s, t);
-            
-            Assert.Equal<bool>(expectedResult, actualResult);
-        }
-        
-        [Theory]
         [InlineData("abcdefhi", "afi", "abcdefhi")]
         [InlineData("abcdefhi", "aef", "abcdef")]
         [InlineData("zwsxf", "abc", "")]
         [InlineData("zwsxf", "abcdefghij", "")]
+        [InlineData("bba", "ab", "ba")]
         public void TestMinWindow(string s, string t, string expectedResult)
         {
             var stringProblems = new StringProblems();
--- Google/ArraysStrings/StringProblems.cs
+++ Google/ArraysStrings/StringProblems.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Google.ArraysStrings
 {
@@ -11,13 +12,38 @@ namespace Google.ArraysStrings
             {
                 return string.Empty;
             }
-            
+
             // we want to initialize the window to the minimum length of T
             var left = 0;
             var right = t.Length - 1;
             var minSize = 0;
             var minLeft = 0;
             var curSize = t.Length;
+            var symbolLookup = new Dictionary<char, int>();
+            
+            // generate the T symbols into a lookup table.
+            foreach (var c in t)
+            {
+                if (symbolLookup.ContainsKey(c))
+                {
+                    symbolLookup[c]++;
+                }
+                else
+                {
+                    symbolLookup.Add(c, 1);
+                }
+            }
+
+            // this cache results will allow us to lookup previous results
+            // and which symbols matched. so we don't have to rematch after the first match.
+            // seed the currentMatchedSymbols of the current window.
+            foreach (var c in s.Substring(left, curSize))
+            {
+                if (symbolLookup.ContainsKey(c))
+                {
+                    symbolLookup[c]--;
+                }
+            }
             
             // iterate while:
             //    1. the left position has not proceeded beyond the right size
@@ -25,12 +51,8 @@ namespace Google.ArraysStrings
             //    3. if the current window size must be greater than or equal to the length of T
             while (left <= right && curSize >= t.Length && right < s.Length) 
             {
-                // check to see if this current window matches the symbols.
-                var areAllSymbolsFound = symbolsMatchString(s.Substring(left, curSize), t);
-
                 // if they do match make sure to compare current minimum size and window.
-                // if it is less than the current minimum replace current minimum.
-                if (areAllSymbolsFound)
+                if (symbolLookup.Values.All(count => count <= 0))
                 {
                     // if the minimum size isn't matched or if it is minimum
                     // set the current window size and boundary positions as the new minimum.
@@ -40,11 +62,29 @@ namespace Google.ArraysStrings
                         minLeft = left;
                     }
                     
+                    // if removing the leftmost character,
+                    // make sure to see if the left most character matches
+                    if (symbolLookup.ContainsKey(s[left]))
+                    {
+                        // this looks bad but it actually makes sense we want to keep a 0 or less than 0.
+                        symbolLookup[s[left]]++;
+                    }
+                    
                     // increment the left to make sure that we cannot make it any smaller.
                     left++;
                 }
                 else
                 {
+                    // if appending a new character as the rightmost character if it matches
+                    // add to the cache.
+                    if (right + 1 < s.Length)
+                    {
+                        if (symbolLookup.ContainsKey(s[right + 1]))
+                        {
+                            symbolLookup[s[right + 1]]--;
+                        }
+                    }
+
                     // we were unable to find a match grow the window size.
                     right++;
                 }
@@ -56,30 +96,6 @@ namespace Google.ArraysStrings
             // if the size found is zero by not finding anything, 
             // then we should return empty string, else return the window we found.
             return  (minSize < 1) ? string.Empty : s.Substring(minLeft, minSize);
-        }
-        
-        // search string should match.
-        public bool symbolsMatchString(string substring, string symbols)
-        {
-            // how to match symbols exist in substring
-            foreach (var c in substring)
-            {
-                var position = symbols.IndexOf(c);
-
-                if (position < 0)
-                {
-                    continue;
-                }
-
-                symbols = symbols.Remove(position, 1);
-                
-                if (string.IsNullOrEmpty(symbols))
-                {
-                    return true;
-                }
-            }
-
-            return false;
         }
     }
 }
