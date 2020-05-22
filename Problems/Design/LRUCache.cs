ed84e6c5eecc1f3f90815dcfe3bf661221dfed6e
Jonathan Throne <jthrone@skyslope.com>
2020-05-22 10:44:51 -0700

    Added in a different lru cache implementation.

--- Problems/Design/LRUCache.cs
+++ Problems/Design/LRUCache.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Problems.Design
 {
@@ -25,14 +26,14 @@ namespace Problems.Design
     {
         private readonly Dictionary<int, Cache<int>> storage;
         private readonly int capacity;
-
+        
         public LRUCache(int capacity) 
         {
             // store values.
             this.storage = new Dictionary<int, Cache<int>>(capacity);
             this.capacity = capacity;
         }
-    
+        
         public int Get(int key)
         {
             // try to get the value of the key.
@@ -40,14 +41,13 @@ namespace Problems.Design
             {
                 // tick reference
                 this.storage[key].timestamp = DateTimeOffset.Now.Ticks;
-                
                 return item.value;
             }
             
             // not found.
             return -1;
         }
-    
+        
         public void Put(int key, int value) 
         {
             // if the key exists update the value.
@@ -67,12 +67,13 @@ namespace Problems.Design
                 this.storage.Add(key, new Cache<int>(value));
             }
         }
-
+        
+        // sub-optimal solution O(n) linear
         private void evictEldest()
         {
             var minKey = -1;
             var minTime = long.MaxValue;
-
+            
             foreach (var k in this.storage.Keys)
             {
                 if (minTime > this.storage[k].timestamp)
@@ -81,11 +82,81 @@ namespace Problems.Design
                     minKey = k;
                 }
             }
-
+            
             if (minKey > -1)
             {
                 this.storage.Remove(minKey);
             }
+        }
+    }
+    
+    /**
+     * Your LRUCache object will be instantiated and called as such:
+     * LRUCache obj = new LRUCache(capacity);
+     * int param_1 = obj.Get(key);
+     * obj.Put(key,value);
+     */
+    public class LRUCacheDictionary
+    {
+        private readonly Dictionary<int, Cache<int>> storage;
+        private readonly int capacity;
+        private int size;
+        private readonly SortedDictionary<long, int> expirationTracking;
+        
+        public LRUCacheDictionary(int capacity) 
+        {
+            // store values.
+            this.storage = new Dictionary<int, Cache<int>>(capacity);
+            this.expirationTracking = new SortedDictionary<long, int>();
+            this.capacity = capacity;
+            this.size = 0;
+        }
+    
+        public int Get(int key)
+        {
+            // try to get the value of the key.
+            if (this.storage.TryGetValue(key, out var item))
+            {
+                this.expirationTracking.Remove(this.storage[key].timestamp);
+                this.storage[key].timestamp = DateTimeOffset.Now.Ticks;
+                this.expirationTracking.Add(this.storage[key].timestamp, key);
+                return item.value;
+            }
+            
+            // not found.
+            return -1;
+        }
+    
+        public void Put(int key, int value) 
+        {
+            // if the key exists update the value.
+            if (this.storage.ContainsKey(key))
+            {
+                this.expirationTracking.Remove(this.storage[key].timestamp);
+                this.storage[key] = new Cache<int>(value);
+                this.expirationTracking.Add(this.storage[key].timestamp, key);
+            }
+            else
+            {
+                // we must remove the oldest element, because we are inserting new element.
+                if (this.size + 1 > capacity)
+                {
+                    evictEldest();
+                }
+                
+                // the key is not found insert key -> value pair.
+                this.storage.Add(key, new Cache<int>(value));
+                this.expirationTracking.Add(this.storage[key].timestamp, key);
+                this.size++;
+            }
+        }
+
+        private void evictEldest()
+        {
+            var key = this.expirationTracking.First().Key;
+            this.storage.Remove(this.expirationTracking[key]);
+            this.expirationTracking.Remove(key);
+            this.size--;
         }
     }
 }
